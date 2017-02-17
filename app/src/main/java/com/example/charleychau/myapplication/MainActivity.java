3285fd8daa8895322ee3fedce190f1aafd44028e
charleychau <charleychau81@gmail.com>
2017-02-16 19:10:16 -0500

    Added functionality for Tutorial and Settings buttons

--- app/src/main/java/com/example/charleychau/myapplication/MainActivity.java
+++ app/src/main/java/com/example/charleychau/myapplication/MainActivity.java
@@ -9,8 +9,8 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button playButton;
-    //private Button tutorial_button;
-    //private Button settings_button;
+    private Button tutorialButton;
+    private Button settingsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +18,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         playButton = (Button) findViewById(R.id.playButton);
+        tutorialButton = (Button) findViewById(R.id.tutorialButton);
+        settingsButton = (Button) findViewById(R.id.settingsButton);
         playButton.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v) {
                 Intent playGame = new Intent(MainActivity.this, Game.class);
                 startActivity(playGame);
+            }
+        });
+        tutorialButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent tutorial = new Intent(MainActivity.this, Tutorial.class);
+                startActivity(tutorial);
+            }
+        });
+        settingsButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v) {
+                Intent settings = new Intent(MainActivity.this, Settings.class);
+                startActivity(settings);
             }
         });
     }
