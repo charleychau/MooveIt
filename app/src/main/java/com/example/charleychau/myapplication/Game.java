9336af07301bbe5d77b0048d4478a416f392fcb9
charleychau <charleychau81@gmail.com>
2017-02-18 14:56:31 -0500

    Added countdown timer for game

--- app/src/main/java/com/example/charleychau/myapplication/Game.java
+++ app/src/main/java/com/example/charleychau/myapplication/Game.java
@@ -1,15 +1,30 @@
 package com.example.charleychau.myapplication;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Game extends AppCompatActivity {
+
+    private TextView mTextField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        //TESTasdasa
-        //Test again
+
+        mTextField = (TextView) findViewById(R.id.playTimer);
+        new CountDownTimer(4000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                mTextField.setText(millisUntilFinished / 1000 + "");
+            }
+
+            public void onFinish() {
+                mTextField.setText("Moove It!");
+            }
+        }.start();
+
     }
 }
